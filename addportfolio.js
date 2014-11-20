f05e7aea5f5c9fd86c90f2ebf0fd67c9a6e5ee59
nikkidahan <nicoledahan@Nicoles-MBP-2.home>
2014-11-19 21:27:24 -0500

    Revert 2cc16c3..d9242d3
    This rolls back to commit 2cc16c363466a0919526666b7d16fd5f1966f117.

--- addportfolio.js
+++
@@ -1,8 +0,0 @@
-var xhr= new XMLHttpRequest();
-xhr.open('GET', 'portfolio-section.html', true);
-xhr.onreadystatechange= function() {
-    if (this.readyState!==4) return;
-    if (this.status!==200) return; // or whatever error handling you want
-    document.getElementById('portfolio-section').innerHTML= this.responseText;
-};
-xhr.send();
